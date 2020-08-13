e39056bea637fb0705c363d4ead506bddc6e4e97
mateuszswierczewski <mate9816@gmail.com>
2020-08-13 18:20:53 +0200

    Initial commit

---
+++ src/main/java/pl/mswierczewski/socialwall/SocialWallApplication.java
@@ -0,0 +1,13 @@
+package pl.mswierczewski.socialwall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialWallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialWallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/mswierczewski/socialwall/SocialWallApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mswierczewski.socialwall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialWallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
